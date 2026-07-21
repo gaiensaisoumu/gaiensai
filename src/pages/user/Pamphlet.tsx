2a79c6469eb711bfa5bc72397ff313772a87ecd8
Rio Gunawan <ribbongrapes@gmail.com>
2026-07-22 01:03:55 +0900

    パンフレットページにポスター画像を追加。

--- src/pages/user/Pamphlet.tsx
+++ src/pages/user/Pamphlet.tsx
@@ -1,12 +1,14 @@
 import { useTitle } from '../../hooks/useTitle';
 import styles from '../../styles/sub-pages.module.css';
+import posterImg from '../../assets/poster.webp';
 
 const Pamphlet = () => {
   useTitle('パンフレット');
   return (
     <>
       <h1 className={styles.pageTitle}>パンフレット</h1>
-      <section>準備中</section>
+      <img src={posterImg} alt='外苑祭2026 ポスター' className={styles.poster} />
+      <section>ただいま、パンフレットを作成中です。しばらくお待ちください。</section>
     </>
   );
 };
