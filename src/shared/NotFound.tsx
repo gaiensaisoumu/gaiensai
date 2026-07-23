47ea34d45e50f82885786450de37a106c30fc763
Rio Gunawan <ribbongrapes@gmail.com>
2026-07-24 01:26:21 +0900

    404ページにNoindexタグを追加

--- src/shared/NotFound.tsx
+++ src/shared/NotFound.tsx
@@ -1,32 +1,36 @@
+import { NoIndexMeta } from '../components/NoIndexMeta';
 import styles from './NotFound.module.css';
 
 const NotFound = () => {
   return (
-    <section className={styles.card}>
-      <p className={styles.status}>404 Not Found</p>
-      <h1 className={styles.heading}>ページが見つかりません</h1>
-      <p className={styles.description}>
-        お探しのページは削除されたか、URLが間違っている可能性があります。
-      </p>
-      <p className={styles.description}>
-        リンクをたどるか、ホームに戻って別のページをご覧ください。
-      </p>
-      <p>
-        <span
-          style={{
-            backgroundColor: '#000',
-            color: '#00ff33',
-            fontFamily:
-              'Hack, HackGen, "Cascadia Code", Consolas, Monaco, monospace',
-          }}
-        >
-          HINT: Are you looking for https:// ... /gunawan ?
-        </span>
-      </p>
-      <a className={styles.returnLink} href='/'>
-        ホームに戻る
-      </a>
-    </section>
+    <>
+      <NoIndexMeta />
+      <section className={styles.card}>
+        <p className={styles.status}>404 Not Found</p>
+        <h1 className={styles.heading}>ページが見つかりません</h1>
+        <p className={styles.description}>
+          お探しのページは削除されたか、URLが間違っている可能性があります。
+        </p>
+        <p className={styles.description}>
+          リンクをたどるか、ホームに戻って別のページをご覧ください。
+        </p>
+        <p>
+          <span
+            style={{
+              backgroundColor: '#000',
+              color: '#00ff33',
+              fontFamily:
+                'Hack, HackGen, "Cascadia Code", Consolas, Monaco, monospace',
+            }}
+          >
+            HINT: Are you looking for https:// ... /gunawan ?
+          </span>
+        </p>
+        <a className={styles.returnLink} href='/'>
+          ホームに戻る
+        </a>
+      </section>
+    </>
   );
 };
 
